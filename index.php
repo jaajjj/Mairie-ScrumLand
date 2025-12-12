cc26cbe318c56957988d619e6b6b15dbf027ed71
Sami <samihajadi@gmail.com>
2025-12-12 09:15:53 +0100

    creation fichier de bases

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'vendor/autoload.php';
+
+session_start();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$page = $_GET['page'] ?? 'accueil';
+switch($page) {
+    case 'inscription':
+        
+            require_once __DIR__ . '/views/templates/header.php';
+            require_once __DIR__ . '/views/inscription.php';
+            require_once __DIR__ . '/views/templates/footer.php';
+        
+        break;
+    case 'accueil':
+        require 'views/login.php';
+        break;
+     case 'traitementConnexion':
+        require 'controllers/ConnexionController.php';
+        break;
+    case 'catalogue':
+        require 'views/catalogue.php';
+        break;
+    case 'home':
+        require 'views/home.php';
+        break;
+    case 'detailMusique':
+        require 'views/detailMusique.php';
+        break;
+    case 'compte':
+        require 'views/compte.php';
+        break;
+    case 'bibliotheque':
+        require 'views/bibliotheque.php';
+        break;
+    case 'TraitementDeconnexion':
+        require 'controllers/DeconnexionController.php';
+        break;
+    case 'inscription_traitement':
+        require 'controllers/traitement_Inscription.php';
+        break;
+    default:
+        http_response_code(404);
+        break;
+}
+require_once 'views/templates/footer.php';
