7bf7da507b82c1879c1aca71f4827e09be643b89
Antoine Rioul <rioul.antoine@gmail.com>
2025-12-19 08:48:32 +0100

    Add elus page with elected officials details and timeline; update header links and image paths

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 session_start();
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
+$baseUrl = $_ENV['BASE_URL'];
 $page = $_GET['page'] ?? 'accueil';
 switch($page) {
     case 'accueil':
@@ -12,5 +12,8 @@ switch($page) {
         break;
     case 'contact':
         require 'Views/contact.php';
+        break;
+    case 'elus':
+        require 'Views/elus.php';
         break;
 }
